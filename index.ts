1a8cead1ebadfd9142fca51cd23659332aa3b4b0
Ego Maragustaf <egomaragustaf@gmail.com>
2023-10-31 02:26:34 +0700

    feat: add seeder businesses data

--- index.ts
+++ index.ts
@@ -1,24 +1,26 @@
-import { PrismaClient } from '@prisma/client'
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import { PrismaClient } from "@prisma/client";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('This is Yelp Clone');
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is Yelp Clone");
 });
 
-app.get('/business/search', async (req, res) => {
-    const businesses = await prisma.business.findMany({
-     
-    })
-    res.json(businesses)
-  })
+app.get("/business/search", async (req, res) => {
+  const businesses = await prisma.business.findMany({
+  include: {
+    locations: true,
+  }
+  });
+  res.json(businesses);
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import dataBusinesses from "./businesses.json";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const business of dataBusinesses) {
+    // Insert each business object into the database
+    const newBusinesses = await prisma.business.create({
+      data: {
+        alias: business.alias,
+        name: business.name,
+        is_closed: business.is_closed,
+        review_count: business.review_count,
+        rating: business.rating,
+        price: business.price,
+        locations: {
+            create: business.location
+          }
+        },
+        include: {
+          locations: true
+        }
+      });
+
+    console.log("✅ Seeded data:", newBusinesses);
+  }
+}
+
+main()
+  .then(async () => {
+    console.log("✅ Seeding complete");
+    await prisma.$disconnect();
+  })
+  .catch((e) => {
+    console.error(e);
+    console.log("❌ Seeding failed");
+    prisma.$disconnect();
+    process.exit(1);
+  });
