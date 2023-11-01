dd2ee59a31262ae8855f9eefd23e2360a537b436
Ego Maragustaf <egomaragustaf@gmail.com>
2023-11-01 22:55:22 +0700

    feat: routes for add business data

--- index.ts
+++ index.ts
@@ -9,19 +9,8 @@ const prisma = new PrismaClient();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/business", (req: Request, res: Response) => {
   res.send("This is Yelp Clone");
-});
-
-app.delete("/business/:id", async (req: Request, res: Response) => {
-  const { id } = req.params
-  const deleteBusinesses = await prisma.business.delete({
-    where: { id, },
-    include: {
-      locations: true,
-    }
-  });
-  res.json(deleteBusinesses);
 });
 
 app.get("/business/search", async (req: Request, res: Response) => {
@@ -32,6 +21,50 @@ app.get("/business/search", async (req: Request, res: Response) => {
     },
   });
   res.json(businesses);
+});
+
+app.get("/business/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const businessId = await prisma.business.findUnique({
+    where: { id },
+    include: {
+      locations: true,
+    },
+  });
+  res.json(businessId);
+});
+
+// Add data without form
+app.post("/business/add", async (req: Request, res: Response) => {
+  const addBusinesses = await prisma.business.create({
+    data: {
+      alias: "delicious-eats-chicago",
+      name: "Delicious Eats",
+      is_closed: false,
+      review_count: 1500,
+      rating: 4.0,
+      price: "$$$",
+      locations: {
+        create: {
+          address1: "456 Elm St",
+          address2: "Suite 200",
+          city: "Chicago",
+          zip_code: "60601",
+          country: "US",
+          state: "IL",
+        },
+      },
+    },
+  });
+  res.json(addBusinesses);
+});
+
+app.delete("/business/:id/delete", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deleteBusinesses = await prisma.business.delete({
+    where: { id },
+  });
+  res.json(deleteBusinesses);
 });
 
 app.listen(port, () => {
